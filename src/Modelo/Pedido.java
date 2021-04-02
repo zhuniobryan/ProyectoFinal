f61398f30f30dcf3a0f89fcf1f6a69ec69eb2e41
bryan <bryan@DESKTOP-9Q3IP99>
2021-04-02 15:36:35 -0500

    Creación de Clases Bases

--- src/Modelo/Pedido.java
+++ src/Modelo/Pedido.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Vista;
+package Modelo;
 
 /**
  *
  * @author bryan
  */
 public class Pedido {
-    
+
 }
